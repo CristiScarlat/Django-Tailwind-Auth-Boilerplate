af7a92832976dc3545f21f19515cdfb903773f0e
scarlat-trainer <78.cristi@gmail.com>
2023-03-24 09:06:22 +0200

    add register url and template

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 
 @login_required(login_url='/login')
@@ -8,6 +9,6 @@ def index(request):
     return render(request, 'index.html')
 
 
-def login(request):
-    print("login")
-    return
+def register(request):
+    print("signup")
+    return render(request, 'registration/register.html')
