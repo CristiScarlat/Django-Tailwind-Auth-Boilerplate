2690df6bf6183f41886f71497b477ce28e824a9c
scarlat-trainer <78.cristi@gmail.com>
2023-03-26 08:50:54 +0300

    add custom style to register fields and form error display

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
 
 @login_required(login_url='/login')
@@ -10,5 +12,15 @@ def index(request):
 
 
 def register(request):
-    print("signup")
-    return render(request, 'registration/register.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/register.html', {'form': form})
